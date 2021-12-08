a8b8543054b9b3cf67bff2e84f46b05a45aaa161
Andrew Evans <andrewevans0102@gmail.com>
2021-12-08 09:45:24 -0500

    first commit

---
+++ HelloAlias.js
@@ -0,0 +1,7 @@
+const daysUntilChristmas = require("days-until-christmas");
+const daysUntilChristmasNext = require("days-until-christmas-next");
+
+console.log("counting the days until Christmas with the original package");
+console.log(daysUntilChristmas());
+console.log("counting the days until Christmas with the new package");
+console.log(daysUntilChristmasNext());
---
+++ README.md
@@ -0,0 +1,5 @@
+# NPM Aliases
+
+This is a very small project that showcases an npm alias. It shows how you can install different npm packages under different names if you need to refer to them in your project or want to try out a new version.
+
+This project is a companion to my post [creating aliases with npm]().
